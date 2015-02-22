8c0e69736e16fa19c6129a8b34b84ee4eeaabae0
Eugene <banshee1221@gmail.com>
2015-02-22 22:56:47 +0200

    Added home, fixed header | Collapse start
    - Added home button and text to header
    - Started initial collapsible script for sideNav

--- resources/js/init.js
+++ resources/js/init.js
@@ -3,12 +3,14 @@
 
         $('.but-collapse').sideNav({
                 edge: 'left', // Choose the horizontal origin
+                menuWidth: 300,
                 activationWidth: 70,
                 closeOnClick: true
             }
         );
         $('.button-collapse').sideNav({
                 edge: 'left', // Choose the horizontal origin
+                menuWidth: 300,
                 activationWidth: 70,
                 closeOnClick: true
             }
@@ -16,3 +18,9 @@
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+});
